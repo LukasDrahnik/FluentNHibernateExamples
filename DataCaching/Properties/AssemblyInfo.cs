1a9222b44eabfc90a9504af4d992d3a61db163aa
Lukas Drahnik <L.Drahnik@gmail.com>
2014-03-29 12:53:26 +0100

    Init

---
+++ DataCaching/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FluentNHibernateExamples.DataCaching")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FluentNHibernateExamples.DataCaching")]
+[assembly: AssemblyCopyright("Copyright ©  2014 Drahník Lukáš")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("de5a8140-c0fe-41e2-8850-b6e72b1a49c7")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
